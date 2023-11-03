fe354959456337093ba61e9d8298c9f5bfdba3e0
Rui Jin (ext.) <rui.jin@partner.bmw.com>
2023-11-03 17:11:39 +0800

    create ASTgenerator

--- ast/1.ast.js
+++ ast/1.ast.js
@@ -5,36 +5,33 @@ const fs = require('fs');
 const esprima = require('esprima');
 const estraverse = require('estraverse');
 const escodegen = require('escodegen');
-// let code;
-//code = 'function a(){}';
-fs.readFile("textfile.ts", "utf8", function(error, data){
+
+fs.readFile(__dirname + "/textfile.ts", "utf8", function(error, data){
     if(error){
         console.log("Failed to load testfile:", error);
         rejects(error);
         return;
     }
     const ast = esprima.parseScript(data);
-    console.log(ast);
-    console.log(ast.body);
-    console.log(ast.body[0].body);
-    console.log(ast.body[0].body.body);
+
+    visAllnode(ast);
 })
-// code ---> ast
-// const ast = esprima.parseScript(code);
-// console.log(ast);
 
-// vistor mode, one enter, one leave
-// estraverse.traverse(ast, {
-//     enter(node){
-//         console.log('enter:' + node.type)
 
-//         if(node.type == 'FunctionDeclaration'){
-//             node.id.name = 'ast'
-//         }
-//     },
-//     leave(node){
-//         console.log('leave:' + node.type)
-//     }
-// })
-// //console.log(ast);
-// console.log(escodegen.generate(ast));
+function visAllnode(mynode){
+    if(mynode != undefined){
+        console.log(mynode);
+        console.log("#######################")
+        if(mynode.hasOwnProperty("body")){
+            //if(mynode.body.hasOwnProperty("type")){
+                if(mynode.body.type == "ClassBody" || mynode.body.type == "BlockStatement"){
+                    visAllnode(mynode.body);
+                }else{
+                    for(let i = 0; i < mynode.body.length; i++){
+                        visAllnode(mynode.body[i]);
+                    }
+                }
+            //}
+        }
+    }
+}
--- ast/textfile.ts
+++ ast/textfile.ts
@@ -1,3 +1,14 @@
-class Person {
-  constructor() { }
-}
+function bubbleSort(arr) {
+    const n = arr.length;
+    // 外层循环：0 ~ n-1
+    for (let i = 0; i < n; i++) {
+    // 内层循环找到最大值
+      for (let j = 1; j < n - i; j++) {
+        if (arr[j - 1] > arr[j]) {
+          // 交换两个数据
+          [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
+        }
+      }
+    }
+    return arr;
+  }
